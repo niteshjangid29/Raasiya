d41c742650493af51f6ffe31d5e0ecbd970728e5
niteshjangid29 <nitesh_j@ee.iitr.ac.in>
2023-06-12 06:49:02 +0530

    Product Route, Model and Filter, Search, Pagination

---
+++ backend/controllers/productController.js
@@ -0,0 +1,81 @@
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apiFeatures");
+
+// Create Product -- Admin SuperAdmin
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
+
+// Get ALL Products
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+  const resultPerPage = 4;
+  const productsCount = await Product.countDocuments();
+
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeature.query;
+  res.status(200).json({
+    success: true,
+    products,
+    productsCount,
+  });
+});
+
+// Update Product --Admin SuperAdmin
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+  let product = Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+// Delete Product --Admin SuperAdmin
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted suceessfully",
+  });
+});
+
+// Get Product Details
+exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
---
+++ backend/models/productModel.js
@@ -0,0 +1,96 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+  },
+  details: {
+    type: String,
+    required: [true, "Please enter product details"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please enter product category"],
+  },
+  collections: {
+    type: String,
+    required: [true, "Please enter collections name"],
+  },
+  Stock: {
+    type: Number,
+    required: [true, "Please enter product Stock"],
+    maxLength: [4, "Stock cannot exceed 4 characters"],
+    default: 1,
+  },
+  numberOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      //   user: {
+      //     type: mongoose.Schema.ObjectId,
+      //     ref: "User",
+      //     required: true,
+      //   },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  countryOfOrigin: {
+    type: String,
+    default: "India",
+  },
+  packedBy: {
+    type: String,
+    required: true,
+    default: "Raasiya Pvt. Ltd",
+  },
+  //   user: {
+  //     type: mongoose.Schema.ObjectId,
+  //     ref: "User",
+  //     required: true,
+  //   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("Product", productSchema);
